67ed5ccbf34457a32ee4316e11cd48805f43b4a9
Lohit Kolluri <lohitkolluri@gmail.com>
2026-07-18 10:12:39 +0530

    test: add test file with hallucinated imports and secrets for codasaurus review

---
+++ test-fixtures/hallucinated.js
@@ -0,0 +1,8 @@
+import { something } from 'this-package-definitely-does-not-exist';
+import { Component } from 'react';
+const express = require('express');
+
+// TODO: this needs error handling
+const API_KEY = "sk-live-abc123def456ghi789jkl012";
+console.log(API_KEY);
+// trigger
